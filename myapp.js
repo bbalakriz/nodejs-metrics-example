5b39ffd1a617f45b7fb9705522f0a5002428b0a5
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2023-03-28 12:49:10 +0800

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -3,19 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
  
 // Use the prom-client module to expose our metrics to Prometheus
-const client = require('prom-client');
+//const client = require('prom-client');
  
 // enable prom-client to expose default application metrics
-client.collectDefaultMetrics();
+// client.collectDefaultMetrics();
 
 // a custom histogram metric which represents the latency
 // of each call to our API /api/greeting.
-const histogram = new client.Histogram({
-  name: 'hello_duration',
-  help: 'Duration of HTTP requests in ms',
-  labelNames: ['method', 'status_code'],
-  buckets: [0.1, 5, 15, 50, 100, 500]
-});
+// const histogram = new client.Histogram({
+//   name: 'hello_duration',
+//   help: 'Duration of HTTP requests in ms',
+//   labelNames: ['method', 'status_code'],
+//   buckets: [0.1, 5, 15, 50, 100, 500]
+// });
  
 // create the express application
 const app = express();
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api/greeting', (request, response) => {
   // start the timer for our custom metric - this returns a function
   // called later to stop the timer
-  const end = histogram.startTimer();
+//   const end = histogram.startTimer();
   const name = request.query.name ? request.query.name : 'World';
   response.send({content: `Hello, ${name}!`});
   // stop the timer
@@ -35,9 +35,9 @@ app.use('/api/greeting', (request, response) => {
 });
  
 // expose our metrics at the default URL for Prometheus
-app.get('/metrics', (request, response) => {
-  response.set('Content-Type', client.register.contentType);
-  response.send(client.register.metrics());
-});
+// app.get('/metrics', (request, response) => {
+//   response.set('Content-Type', client.register.contentType);
+//   response.send(client.register.metrics());
+// });
  
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
